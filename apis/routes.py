989968cd5d9d3e7d6222dd4e67d9bdb420118f69
Sanjeev Kumar <ksanjeev@zeomega.com>
2018-12-17 17:04:47 +0530

    added 'api' in routes

--- apis/routes.py
+++ apis/routes.py
@@ -1,5 +1,5 @@
 from apis.procedure.controller import add_procedure
 
 def includeme(config):
-	config.add_route('insert_procedure', '/v1.0/procedure')
+	config.add_route('insert_procedure', 'api/v1.0/procedures')
 	config.add_view(add_procedure, renderer="json", route_name='insert_procedure')
